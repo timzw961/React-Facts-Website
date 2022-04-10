2d14e782f20279ff666eac27a12e1f4aee90e047
Timothy <102215468+timzw961@users.noreply.github.com>
2022-04-10 20:58:45 +1000

    adding more "facts"

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,7 +10,18 @@ export default function Main(){
                 <li> Has well over 100k stars on GitHub </li>
                 <li> Is maintained by Facebook </li>
                 <li> Powers thousands of enterprise apps, including mobile apps </li>
-                <li> This is also another test </li>
+                <li> <a href ="https://create-react-app.dev/docs/getting-started/" >Create React App</a> 
+                is an "easy" way of starting your first React Applications.  </li>
+                <li> 
+                    Trying to simply copy and paste the React App folder created from the "create react app" quick start guide into
+                    a GitHub repository doesn't work as you may have thought. 
+                </li>
+                <li> 
+                    You will also need to do some further <a href="https://github.com/gitname/react-gh-pages"> installation</a>. 
+                    These extra steps will also allow you to deploy your website to a GitHub Page! So you can show off your website that you've
+                    created on a GitHub repository, and also by using a GitHub Page. 
+                </li>
+
             </ul>
         </main>
     )
