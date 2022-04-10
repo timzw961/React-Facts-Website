6f230554c545e9e36c11eedbb6cb7f5c11336a0e
Timothy <102215468+timzw961@users.noreply.github.com>
2022-04-10 20:43:29 +1000

    another test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ export default function Footer(){
     return (
         <footer>
             <small> Website developed by Timothy Wang </small>   
+            <small> Website developed by Timothy Wang </small>   
         </footer>
     )
 }
